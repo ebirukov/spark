d70c1802ff2fb3a844b9aee2accc6f293e1e00ad
e.birukov <e.birukov@corpwebgames.com>
2016-01-03 15:23:33 +0300

    add template

--- docker-run-spark-env.sh
+++ docker-run-spark-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cat /usr/local/spark/conf/template-core-site.xml | m4 -DAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -DAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY > /usr/local/spark/conf/core-site.xml
+cat /usr/local/spark/conf/template-hive-site.xml | m4 -DDB_CONNECTION_URL=$DB_CONNECTION_URL -DDB_PASS=$DB_PASS > /usr/local/spark/conf/hive-site.xml
 env | grep SPARK | awk '{print "export \"" $0 "\""}' > /usr/local/spark/conf/spark-env.sh
 
 exec $@
