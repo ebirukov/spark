57ee4c53c6578d9f607a3d1d53548002f15044a1
e.birukov <e.birukov@corpwebgames.com>
2016-01-03 15:06:38 +0300

    template

--- docker-run-spark-env.sh
+++ docker-run-spark-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat /usr/local/spark/conf/core-site.xml | m4 -DAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -DAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY > /usr/local/spark/conf/core-site.xml
+cat /usr/local/spark/conf/template-core-site.xml | m4 -DAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -DAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY > /usr/local/spark/conf/core-site.xml
 env | grep SPARK | awk '{print "export \"" $0 "\""}' > /usr/local/spark/conf/spark-env.sh
 
 exec $@
