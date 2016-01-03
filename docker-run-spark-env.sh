8dae23475c2aff4659064ab289be8cddadf6f5a7
e.birukov <e.birukov@corpwebgames.com>
2016-01-03 19:53:54 +0300

    export

--- docker-run-spark-env.sh
+++ docker-run-spark-env.sh
@@ -4,6 +4,8 @@ export SPARK_MASTER_PORT=7077
 export  SPARK_MASTER_WEBUI_PORT=8080
 export SPARK_WORKER_PORT=8888
 export SPARK_WORKER_WEBUI_PORT=8081
+export SPECIAL_SPARK_OPTS="-Dspark.driver.port=7001 -Dspark.fileserver.port=7002 -Dspark.broadcast.port=7003 -Dspark.replClassServer.port=7004 -Dspark.blockManager.port=7005 -Dspark.executor.port=7006 -Dspark.ui.port=4040 -Dspark.broadcast.factory=org.apache.spark.broadcast.HttpBroadcastFactory $SPECIAL_SPARK_OPTS"
+
 export SPARK_MASTER_OPTS="$SPECIAL_SPARK_OPTS"
 export SPARK_WORKER_OPTS="$SPECIAL_SPARK_OPTS"
 export SPARK_JAVA_OPTS="$SPECIAL_SPARK_OPTS"
