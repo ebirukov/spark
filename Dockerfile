28b1caf57c9f0e24a0c0801b582c3f803d56d0b0
e.birukov <e.birukov@corpwebgames.com>
2016-01-04 12:03:46 +0300

    refactor

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,4 @@ RUN apt-get update \
         && chmod +x /etc/profile.d/configure.sh
 
 ADD docker-run-spark-env.sh /usr/local/bin/docker-run-spark-env.sh
-ADD template-hive-site.xml /usr/local/spark/conf/template-hive-site.xml
-ADD template-core-site.xml /usr/local/spark/conf/template-core-site.xml
 ADD mysql-connector-java-5.1.30-bin.jar usr/local/spark/lib/
--- docker-run-spark-env.sh
+++ docker-run-spark-env.sh
@@ -14,9 +14,9 @@ export SPARK_MASTER_IP=$DOCKER_HOSTNAME
 export SPARK_LOCAL_IP=$DOCKER_HOSTNAME
 export SPARK_LOCAL_HOSTNAME=$DOCKER_HOSTNAME
 
-cat /usr/local/spark/conf/template-core-site.xml | m4 -DAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -DAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY > /usr/local/spark/conf/core-site.xml
-#cat /usr/local/spark/conf/template-hive-site.xml | m4 -DDB_CONNECTION_URL=$DB_CONNECTION_URL -DDB_PASS=$DB_PASS > /usr/local/spark/conf/hive-site.xml
-aws s3 cp s3://webgames-emr/hive/config/hive-site.xml /usr/local/spark/conf/hive-site.xml > /dev/null
+#cat /usr/local/spark/conf/template-core-site.xml | m4 -DAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -DAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY > /usr/local/spark/conf/core-site.xml
+aws s3 cp s3://$S3_BUCKET_CONF/hive/config/core-site.xml /usr/local/spark/conf/core-site.xml > /dev/null
+aws s3 cp s3://$S3_BUCKET_CONF/hive/config/hive-site.xml /usr/local/spark/conf/hive-site.xml > /dev/null
 env | grep SPARK | awk '{print "export \"" $0 "\""}' > /usr/local/spark/conf/spark-env.sh
 export CLASSPATH="/usr/local/spark/lib/mysql-connector-java-5.1.30-bin.jar"
 export PATH="$PATH:/usr/local/spark/bin"
